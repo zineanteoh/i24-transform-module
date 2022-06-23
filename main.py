6089cdd14fcf38961e6641ac50091aef7abd6563
Zi Nean Teoh <zinean00@gmail.com>
2022-06-23 14:00:38 -0500

    complete basic multiprocessing setup

--- main.py
+++ main.py
@@ -4,8 +4,42 @@ Created on Thu Jun 23, 2022
 @author: teohz
 """
 
+# from i24_database_api.DBReader import DBReader
+from multiprocessing import Process, Queue
 import transform
+import json
+
+def initialize_DB(config):
+    return 
 
 if __name__=="__main__":
-    obj1 = transform.Transform()
-    obj1.transform()
+    # (Optional) Uncomment the following two lines if config.json cannot be loaded 
+    # path_to_directory = "/isis/home/teohz/Desktop/i24-transform-module"
+    # os.chdir(path_to_directory)
+
+    # Load config file from json
+    with open('config.json') as f:
+        config = json.load(f)
+    
+    initialize_DB(config)
+
+    # change_stream_reader pushes trajectories to this queue, which transform would listen from
+    change_stream_connection = Queue()
+    # transform pushes mongoDB operation requests to this queue, which batch_update would listen from
+    batch_update_connection = Queue()
+    
+    print("[Main] Starting Change Stream process...")
+    proc_change_stream = Process(target=None, args=(change_stream_connection, ))
+    proc_change_stream.start()
+    
+    print("[Main] Starting Transformation process...")
+    proc_transform = Process(target=None, args=(change_stream_connection, ))
+    proc_transform.start()
+
+    print("[Main] Starting Batch Update process...")
+    proc_batch_update = Process(target=None, args=(conn, ))
+    proc_batch_update.start()
+    
+    proc_change_stream.join()
+    proc_transform.join()
+    proc_batch_update.join()
--- transform.py
+++ transform.py
@@ -4,10 +4,19 @@ Created on Thu Jun 23, 2022
 @author: teohz
 """
 
-class Transform:
-    def __init__(self):
-        self.myVar = "hello"
+import batch_update
 
-    def transform(self):
+class Transform:
+    def __init__(self, batch_update):
+        # cache for storing the trajectories
+        self.cache = {}
+        # sample rate per second (<= 30)
+        self.sample_rate = 30
+        # batch update object
+        self.batch_update = batch_update
+
+    def transform_trajectory(self, traj):
+        vehicle_id = traj["id"]
+        
         print("transformed!")
         return 
