303e34e9371a0833c62fb1af5c36cb053f6b3eae
Zi Nean Teoh <zinean00@gmail.com>
2022-06-23 16:50:44 -0500

    complete high level functionality

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ Created on Thu Jun 23, 2022
 
 # from i24_database_api.DBReader import DBReader
 from multiprocessing import Process, Queue
-import transformation
+from transformation import *
 import json
 
-def initialize_DB(config):
-    return 
+def initialize_transformation():
+    return
+
+def test_function():
+    arr_length = 30
+    test_traj = {
+        "_id": 'ObjectId("62a37ac518c10e37c2103c78")',
+        "timestamp": [x/30 for x in range(arr_length)], 
+        "x_position": [x for x in range(arr_length)],
+        "y_position": [10 for x in range(arr_length)],
+        "coarse_vehicle_class": 1,
+        "first_timestamp": 0,
+        "last_timestamp": 0.2,
+    }
+    testObj = Transformation()
+    testObj.transform_trajectory(test_traj)
 
 if __name__=="__main__":
     # (Optional) Uncomment the following two lines if config.json cannot be loaded 
@@ -20,26 +34,35 @@ if __name__=="__main__":
     # Load config file from json
     with open('config.json') as f:
         config = json.load(f)
-    
-    initialize_DB(config)
 
-    # change_stream_reader pushes trajectories to this queue, which transform would listen from
-    change_stream_connection = Queue()
-    # transform pushes mongoDB operation requests to this queue, which batch_update would listen from
-    batch_update_connection = Queue()
-    
-    print("[Main] Starting Change Stream process...")
-    proc_change_stream = Process(target=None, args=(change_stream_connection, ))
-    proc_change_stream.start()
-    
-    print("[Main] Starting Transformation process...")
-    proc_transform = Process(target=None, args=(change_stream_connection, batch_update_connection, ))
-    proc_transform.start()
+    testing = True
+    if testing:
+        test_function()
+    else:
+        # initialize Transformation, BatchUpdate, and ChangeStreamReader
+        transformation_obj = Transformation()
+        # batch_update_obj = BatchUpdate()
+        # chg_stream_reader_obj = ChangeStreamReader()
 
-    print("[Main] Starting Batch Update process...")
-    proc_batch_update = Process(target=None, args=(batch_update_connection, ))
-    proc_batch_update.start()
-    
-    proc_change_stream.join()
-    proc_transform.join()
-    proc_batch_update.join()
+        # initialize Queue for multiprocessing
+        # - change_stream_reader pushes trajectories to this queue, which transform would listen from
+        # - transform pushes mongoDB operation requests to this queue, which batch_update would listen from
+        change_stream_connection = Queue()
+        batch_update_connection = Queue()
+        
+        # start all 3 child processes
+        print("[Main] Starting Change Stream process...")
+        proc_change_stream = Process(target=None, args=(change_stream_connection, ))
+        proc_change_stream.start()
+        
+        print("[Main] Starting Transformation process...")
+        proc_transform = Process(target=transformation_obj.main_loop, args=(change_stream_connection, batch_update_connection, ))
+        proc_transform.start()
+
+        print("[Main] Starting Batch Update process...")
+        proc_batch_update = Process(target=None, args=(batch_update_connection, ))
+        proc_batch_update.start()
+        
+        proc_change_stream.join()
+        proc_transform.join()
+        proc_batch_update.join()
--- transformation.py
+++ transformation.py
@@ -4,19 +4,71 @@ Created on Thu Jun 23, 2022
 @author: teohz
 """
 
-import batch_update
-
 class Transformation:
-    def __init__(self, batch_update):
-        # cache for storing the trajectories
-        self.cache = {}
-        # sample rate per second (<= 30)
-        self.sample_rate = 30
-        # batch update object
-        self.batch_update = batch_update
+    def __init__(self, sample_rate = 30):
+        # sample rate per second (must be a positive factor of 30)
+        self.SAMPLE_RATE = sample_rate
+        self.valid_sample_rate = 30 % self.SAMPLE_RATE == 0 and self.SAMPLE_RATE > 0
+        if (not self.valid_sample_rate):
+            print("[Transformation] Init ERROR: Sample rate is not valid. Needs to be a positive factor of 30")
 
     def transform_trajectory(self, traj):
-        vehicle_id = traj["id"]
+        """
+        Accepts a trajectory document as parameter and return a dictionary of 
+        timestamps with the following schema: 
+            {
+                t1: [id1, x1, y1],
+                t2: [id2, x2, y2],
+                ...
+            }
+        where 
+        - id is the vehicle's ObjectID from mongoDB, 
+        - x, y are the positions of the vehicle at time t1
+        - t1 is the timestamp
+        """
+        if not self.valid_sample_rate:
+            print("[Transformation] Transform ERROR: Sample rate is not valid. Needs to be a positive factor of 30")
+            return
 
-        print("transformed!")
-        return 
+        vehicle_id = traj["_id"]
+        batch_operations = {}
+        c1 = 0
+        c2 = 0
+        for i in range(len(traj["timestamp"])):
+            # TODO: need to normalize time and skip objects based on frame rate
+            time = traj["timestamp"][i]
+            if time % (1 / self.SAMPLE_RATE) != 0:
+                print("NOT ZERO (time: {})".format(time))
+                c1 += 1
+            else:
+                print("ZERO (time: {})".format(time))
+                c2 += 1
+            x = traj["x_position"][i]
+            y = traj["y_position"][i]
+            batch_operations[time] = [vehicle_id, x, y]
+        print("score: {} vs {}".format(c1, c2))
+        # print("\ntransformation complete")
+        # for op in batch_operations:
+        #     print("dict[{}]: {}".format(op, batch_operations[op]))
+        return batch_operations
+
+    def main_loop(self, change_stream_connection, batch_update_connection):
+        """
+        A child process for transformation. 
+        1. Listens to change_stream_connection for trajectory documents. 
+        2. Transforms the received trajectory into a dictionary of timestamps: 
+            {
+                t1: [id1, x1, y1],
+                t2: [id2, x2, y2],
+                ...
+            }
+        3. Sends the dictionary of timestamp to batch_update
+        """
+        if not self.valid_sample_rate:
+            print("[Transformation] Main Loop ERROR: Sample rate is not valid. Needs to be a positive factor of 30")
+            return
+
+        while True:
+            traj_doc = change_stream_connection.get()
+            batch_operations = self.transform_trajectory(traj_doc)
+            batch_update_connection.send(batch_operations)
