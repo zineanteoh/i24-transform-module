ef873d2e39020d4ad96e3e6a0cb174b0e9743abe
Zi Nean Teoh <zinean00@gmail.com>
2022-06-23 11:56:53 -0500

    add main and transform

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Created on Thu Jun 23, 2022
+
+@author: teohz
+"""
+
+import transform
+
+if __name__=="__main__":
+    obj1 = transform.Transform()
+    obj1.transform()
---
+++ transform.py
@@ -0,0 +1,13 @@
+"""
+Created on Thu Jun 23, 2022
+
+@author: teohz
+"""
+
+class Transform:
+    def __init__(self):
+        self.myVar = "hello"
+
+    def transform(self):
+        print("transformed!")
+        return 
